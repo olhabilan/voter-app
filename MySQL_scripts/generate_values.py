1e855873d27aebeae36cf8139e743ae93db4da14
Olha Bilan <olha.bilan@gmail.com>
2018-04-16 21:43:38 +0300

    Add schema for postgresql

--- MySQL_scripts/generate_values.py
+++
@@ -1,5 +0,0 @@
-import string, random, time
-from random import randint
-END = input("Please enter count of raws:") or 1
-for i in range(0, int(END)):
-    print ('(\"'+''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=8))+'\", '+str(randint(1, 6)) +', \"'+ time.strftime('%Y-%m-%d %H:%M:%S') +'\"),'  )
---
+++ db_scripts/schema_postgresql.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE poll_result;
+USE poll_result;
+GO;
+CREATE TABLE IF NOT EXISTS poll result
+(
+    supermarket_id int NOT NULL AUTO_INCREMENT,
+    supermarket_name CHAR(30) NOT NULL,
+    avg_price_score float,
+    avg_service_score float,
+    avg_overall_score float,
+    PRIMARY KEY (supermarket_id)
+);
+
