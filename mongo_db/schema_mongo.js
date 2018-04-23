e05abb42cefc6c9b6df84fb28f557c6a8f555bec
Olha Bilan <olha.bilan@gmail.com>
2018-04-23 11:21:48 +0300

    update db scripts

--- mongo_db/schema_mongo.js
+++ mongo_db/schema_mongo.js
@@ -10,3 +10,35 @@ db.scoreCollection.insert({ supermarketName: "Green Land",
     isProcessed: false } )
 db.scoreCollection.explain().find()
 db.scoreCollection.getIndexes()
+
+
+
+
+db.scoreCollection.insert({ supermarketName: "Green Land23",
+    codeName: "D870FTfU",
+    serviceScore: 2,
+    overallScore: 4,
+    priceScore: 5,
+    scoreDate: new Date(),
+    isProcessed: false } )
+db.scoreCollection.insert({ supermarketName: "Green Land14",
+    codeName: "D870FTfU",
+    serviceScore: 2,
+    overallScore: 4,
+    priceScore: 5,
+    scoreDate: new Date(),
+    isProcessed: false } )
+db.scoreCollection.insert({ supermarketName: "Green Land",
+    codeName: "D870FTfU",
+    serviceScore: 2,
+    overallScore: 5,
+    priceScore: 5,
+    scoreDate: new Date(),
+    isProcessed: false } )
+db.scoreCollection.insert({ supermarketName: "Green Land14",
+    codeName: "D870FTfU",
+    serviceScore: 2,
+    overallScore: 2,
+    priceScore: 5,
+    scoreDate: new Date(),
+    isProcessed: false } )
