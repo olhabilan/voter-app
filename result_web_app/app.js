e3c4c129066a7e905d4b494219ce7670e8fadd0d
olhabilan <olha.bilan@gmail.com>
2018-05-20 11:19:11 +0300

    add work with postgresql

--- result_web_app/app.js
+++ result_web_app/app.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const db = require('pg');
 const app = new express();
+const connectionString = process.env.DATABASE_URL || 'postgres://gwpafabl:-5ZaqbuJee1ug6h4roma79HI47NOej6S@elmer.db.elephantsql.com:5432/gwpafabl';
+const client = new db.Client(connectionString);
 
 app.get('/', (req, res) => {
 
   res.sendFile(__dirname + '/web/index.html');
 });
 
-app.get('/result', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({shops: [
-    {name: "Silpo", price: 12, service: 13, overall: 10},
-    {name: "Atb", price: 12, service: 13, overall: 10},
-  ]}));
+app.get('/result', async (req, res, next) => {
+  try {
+    await client.connect();
+    const query = await client.query('select * from result;');
+    let shops = query.rows.map((val, index) => {
+      return {
+        name: val.supermarket_name.trim(), 
+        price: val.avg_price_score, 
+        service: val.avg_service_score,
+        overall: val.avg_overall_score,
+        count: val.count
+      };
+    });
+    await client.end();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({shops: shops}));
+  }
+  catch(e) {
+    next(e);
+  }
 });
 
 app.use(express.static(__dirname + '/web'));
